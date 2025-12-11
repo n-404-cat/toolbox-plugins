2077fb960a6cd27de754be44bf331a5d9d5e6769
miinno <shiqingqing@miinno.com>
2025-12-11 18:16:11 +0800

    fix：新增颜色生成，包含HEX颜色、RGB颜色、HSL颜色

---
+++ src/aoaostar_com/color_convert/App.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace plugin\aoaostar_com\color_convert;
+
+use plugin\Drive;
+
+class App implements Drive
+{
+
+    public function Index()
+    {
+        return success();
+    }
+}
